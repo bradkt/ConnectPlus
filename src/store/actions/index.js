df5f1b51015fe4566df8cc3c8d8910daa46eb1c1
Brad Tracy <btracy@everesttech.com>
2019-04-03 12:27:40 -0400

    updating action setlocation

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,4 +1,4 @@
 export { tryAuth, authGetToken, authAutoSignIn, authLogout } from "./auth";
 export { uiStartLoading, uiStopLoading } from "./ui";
-export { getLocation } from "./location";
+export { setLocation } from "./location";
 export { getDevices, addScan } from "./devices";
--- src/store/actions/location.js
+++ src/store/actions/location.js
@@ -1,29 +1,12 @@
 import { SET_LOCATION } from './actionTypes';
 import { uiStartLoading, uiStopLoading, authGetToken } from './index';
 
-export const setLocation = () => {
-    console.log("setting Location");
-    // return dispatch => {
-    //   dispatch(authGetToken())
-    //     .then(token => {
-    //       return fetch(
-    //         "https://fb-rnplay.firebaseio.com/places.json?auth=" +
-    //           token
-    //       );
-    //     })
-    //     .catch(() => {
-    //       alert("No valid token found!");
-    //     })
-    //     .then(res => {
-    //       if (res.ok) {
-    //         return res.json();
-    //       } else {
-    //         throw new Error();
-    //       }
-    //     })
-    //     .catch(err => {
-    //       alert("Something went wrong, could not get place :/");
-    //       console.log(err);
-    //     });
-    // };
+export const setLocation = (coords) => {
+  console.log("setting places: ", coords)
+  return dispatch => {
+    dispatch(SET_LOCATION(coords));
+    // AsyncStorage.setItem("ap:auth:token", token);
+    // AsyncStorage.setItem("ap:auth:expiryDate", expiryDate.toString());
+    // AsyncStorage.setItem("ap:auth:refreshToken", refreshToken);
   };
+};
